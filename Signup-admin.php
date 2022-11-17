e510c21bec386a4efbcae3f316370e47ebf7c490
Charles020702 <valdezcharles0707@gmail.com>
2022-11-17 11:25:55 +0800

    initial commit

---
+++ Signup-admin.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/Signup.css">
+    <link rel = "shortcut icon" type="image" href="img/ResortPic.png">
+    <title>Winesa Reservation System</title>
+</head>
+<body background = "img/Light blue.jpg" class="Backgroundpic">
+<nav class="navbar navbar-expand-lg">
+<h1>Winesa Farm Resort</h1>
+<div class="NavWords">
+<div class="container-fluid">
+    <a class="navbar-brand text-white" href="index.php">Home</a>
+    <a class="navbar-brand text-white" href="LoginAs.php">Login</a>
+    <a class="navbar-brand text-white" href="SignupAs.php">Sign up</a>
+        </li>
+        </div>
+    </div>
+  </div>
+</nav>
+
+<div class = "loginbox">
+<img src = "img/LoginPerson.jpg" class = "avatar">
+<h2 class="text-center">Signup</h2>
+<h4 class="text-center">It's free and only takes a minute</h4>
+<form method="post">
+<p>First Name</p>
+ <input type ="text" name="" placeholder="Enter Firstname">
+<p>Last Name</p>
+<input type = "text" name="" placeholder="Enter Lastname">
+<p>Email</p>
+<input type = "email" name="" placeholder="Enter Email">
+<p>Password</p>
+<input type = "password" name="" placeholder="Enter Password">
+<p>Confirm Password</p>
+<input type = "password" name="" placeholder="Confirm your Password">
+<div class="d-grid gap-2  mx-auto">
+<button type="submit" class="btn btn-outline-secondary rounded-pill btn-lg">Signup</button>
+</div>
+
+<p>By clicking the Sign up button, you agree to our<br>
+<a href="#">Terms and Condition</a> and <a href="#">Policy Privacy</a>
+</p>
+<p class="para2">Already have an account? <a href="#">Login here</a></p> 
+</div>
+
+</form>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
---
+++ SignupAs.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">    
+    <link rel="stylesheet" href="css/LoginAs.css">
+    <link rel = "shortcut icon" type="image" href="img/ResortPic.png">
+    <title>Winesa Reservation System</title>
+</head>
+<body background = "img/Light blue.jpg" class="Backgroundpic">
+<nav class="navbar navbar-expand-lg">
+<h1>Winesa Farm Resort</h1>
+<div class="NavWords">
+<div class="container-fluid">
+    <a class="navbar-brand text-white" href="index.php">Home</a>
+    <a class="navbar-brand text-white" href="LoginAs.php">Login</a>
+    <a class="navbar-brand text-white" href="SignupAs.php">Sign up</a>
+        </li>
+        </div>
+    </div>
+  </div>
+</nav>
+
+
+<div class = "banner">
+<h2>Signup As:</h2>
+<ul>
+<button type = "button"><span></span><a style = "color:white" href = "Signup-client.php"><i class="bi bi-people-fill"></i>Client/Customer</button></a>
+<button type = "button"><span></span><a style = "color:white" href = "Signup-admin.php"><i class="bi bi-laptop-fill"></i>Admin/Owner</button></a>
+</ul>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
